bfd3d434c5e8dc0b04030239847c1cf0fd8b0c34
Richard Williams <richard.williams@manchester.ac.uk>
2020-07-16 13:36:35 +0100

    switched to eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-  extends: [
-    "airbnb-base"
-  ],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['prettier', 'eslint:recommended'],
+  plugins: ['prettier'],
   rules: {
-    "comma-dangle": ["error", {
-      arrays: "always-multiline",
-      objects: "always-multiline",
-      imports: "always-multiline",
-      exports: "always-multiline",
-      functions: "never"
-    }],
-    "no-param-reassign": ["error", { "props": false }],
-    "no-underscore-dangle": ["error", { "allow": ["_id"] }],
-    "object-curly-newline": ["error", {
-      ObjectExpression: { multiline: true },
-      ObjectPattern: { multiline: true }
-    }]
-  }
+    'prettier/prettier': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
 };
